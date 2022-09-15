70e8d510a5b8482e52db40484cdb13a3904a5e36
Onojeta Brown <onosbrown.saved@gmail.com>
2022-09-15 22:00:30 +0100

    first comit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Markup from './markup/Markup';
+import './css/plugins.css';
+import './css/style.css';
+import './css/templete.css';
+import './css/skin/skin-1.css'; 
+
+import './plugins/bootstrap-select/bootstrap-select.min.css';
+import './plugins/slick/slick.min.css';
+import 'react-modal-video/css/modal-video.min.css';
+
+
+function App() {
+	return (			
+		<div className="App">
+			<Markup />
+		</div>	
+	);
+}
+
+export default App;
